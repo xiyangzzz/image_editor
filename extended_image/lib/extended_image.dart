5d7a0358220587bee7a32ccafbe3c18f1c33443b
weiping <1547872843@qq.com>
2022-05-17 19:52:32 +0800

    upload

---
+++ extended_image/lib/extended_image.dart
@@ -0,0 +1,19 @@
+library extended_image;
+
+export 'package:extended_image_library/extended_image_library.dart'
+    hide File, FileImage;
+
+export 'src/border_painter.dart';
+export 'src/editor/editor.dart';
+export 'src/editor/editor_utils.dart';
+export 'src/extended_image.dart';
+export 'src/gesture/gesture.dart';
+export 'src/gesture/page_view/gesture_page_view.dart';
+export 'src/gesture/slide_page.dart';
+export 'src/gesture/slide_page_handler.dart';
+export 'src/gesture/utils.dart';
+export 'src/image/painting.dart';
+export 'src/image/raw_image.dart';
+export 'src/image/render_image.dart';
+export 'src/typedef.dart';
+export 'src/utils.dart';
---
+++ extended_image/test/extended_image_test.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:extended_image/extended_image.dart';
